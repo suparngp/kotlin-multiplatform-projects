1e4b63162715866fffd305ab709faf355e838c6f
Suparn <suparngp@users.noreply.github.com>
2019-07-24 18:43:35 -0700

    Update concurrency for 1.0.5 #20

--- concurrency/build.gradle.kts
+++ concurrency/build.gradle.kts
@@ -4,18 +4,18 @@ plugins {
     id("suparnatural-project")
 }
 
-version = "1.0.4"
+version = "1.0.5"
 
 suparnatural {
     name = "suparnatural-concurrency"
-    description = "Supercharged Kotlin multiplatform threading for iOS and Android."
+    description = "Supercharged Kotlin Multiplatform concurrency/threading for iOS and Android."
     docsUrl = "https://suparngp.github.io/kotlin-multiplatform-projects/concurrency/docs/concurrency/com.suparnatural.core.concurrency/index.html"
     vcsUrl = "https://github.com/suparngp/kotlin-multiplatform-projects/tree/master/concurrency"
     versionLabel = project.version.toString()
     supportsAndroid = true
     supportsCocoapods = true
     supportsIos = true
-    buildNumber = 4
+    buildNumber = 5
     bintray {
         publish = true
         repository = extra[ProjectConfig.Properties.bintrayRepository]!!.toString()
