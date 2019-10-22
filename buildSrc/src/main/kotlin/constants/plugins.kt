2091ac0599795fa75d05f8cfc97fac9afd24c69a
Suparn Gupta <suparngupta+developer@icloud.com>
2019-10-21 20:59:49 -0700

    Updated android versions

--- buildSrc/src/main/kotlin/constants/plugins.kt
+++ buildSrc/src/main/kotlin/constants/plugins.kt
@@ -9,7 +9,7 @@ object Plugins {
     val mavenPublish = Plugin("maven-publish")
     val idea = Plugin("idea")
     val multiplatform = Plugin("kotlin-multiplatform", kotlinVersion, group = "org.jetbrains.kotlin", name = "kotlin-gradle-plugin")
-    val android = Plugin("com.android.library", "3.4.1", group = "com.android.tools.build", name = "gradle")
+    val android = Plugin("com.android.library", "3.5.1", group = "com.android.tools.build", name = "gradle")
     val cocoapods = Plugin("org.jetbrains.kotlin.native.cocoapods", kotlinVersion)
     val dokka = Plugin("org.jetbrains.dokka", "0.9.18", group = "org.jetbrains.dokka", name = "dokka-gradle-plugin")
     val bintray = Plugin("com.jfrog.bintray", "1.8.4", group = "com.jfrog.bintray.gradle", name = "gradle-bintray-plugin")
