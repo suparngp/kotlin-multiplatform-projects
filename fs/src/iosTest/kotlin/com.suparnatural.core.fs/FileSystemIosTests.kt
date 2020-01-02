b0e945e756f3e1679367dca1e31ef4158b17dcc0
Suparn Gupta <suparngupta+developer@icloud.com>
2020-01-01 22:01:57 -0800

    fixed urlFromString

--- fs/src/iosTest/kotlin/com.suparnatural.core.fs/FileSystemIosTests.kt
+++ fs/src/iosTest/kotlin/com.suparnatural.core.fs/FileSystemIosTests.kt
@@ -1,6 +1,7 @@
 package com.suparnatural.core.fs
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
@@ -21,6 +22,27 @@ class FileSystemIosTests {
             assertNotNull(it.relativePath)
             assertTrue(it.absolutePath!!.component!!.isNotEmpty())
             assertTrue(it.relativePath!!.component!!.isNotEmpty())
+        }
+    }
+
+    @Test
+    fun testSimplifiedPath() {
+        val paths = mapOf("/" to "/", "/var" to "/var/", "/var/file.txt" to "/var/file.txt")
+        paths.keys.forEach {
+            val path = Path.simplified(it)
+            assertEquals(it, path.absolutePath!!.component!!)
+            assertEquals(it, path.relativePath!!.component!!)
+            assertEquals("file://${paths[it]}", path.absolutePath!!.url!!.absoluteString)
+            assertEquals("file://${paths[it]}", path.relativePath!!.url!!.absoluteString)
+        }
+    }
+
+    @Test
+    fun testUrlFromString() {
+        val paths = mapOf("/" to "/", "/var" to "/var/", "/.var" to "/.var", "/var/file.txt" to "/var/file.txt")
+        paths.keys.forEach {
+            val url = Path.urlFromString(it)!!
+            assertEquals("file://${paths[it]}", url.absoluteString)
         }
     }
 
